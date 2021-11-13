888a759623b27d05f5a86bc4592efdd73db20891
Teresa Nguyen <teresanguyenpl@gmail.com>
2021-11-13 14:19:20 -0800

    added contact  routes to index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,11 +6,13 @@ const registerRoutes = require('./register');
 const verificationRoutes = require('./verification');
 const signInRoutes = require('./signin');
 const passwordResetRoutes = require('./passwordreset');
+const contactRoutes = require('./contact');
 
 router.use("/passwordreset",passwordResetRoutes)
 router.use("/register", registerRoutes);
 router.use("/signin", signInRoutes);
 router.use("/verification", verificationRoutes);
+router.use("/contact", contactRoutes);
 
 module.exports = router;
 
