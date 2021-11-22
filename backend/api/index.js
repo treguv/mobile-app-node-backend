3177f99d218032e57bfe861e520aaf3f5a13ed88
ScubbyCrub <73358817+ScubbyCrub@users.noreply.github.com>
2021-11-21 17:01:38 -0800

    Lab 5 backend integrated, file directories fixed
    Lab 5 files integrated into the project. File directories in "require" statements fixed to reflect current backend.

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,11 +6,19 @@ const registerRoutes = require('./register');
 const verificationRoutes = require('./verification');
 const signInRoutes = require('./signin');
 const passwordResetRoutes = require('./passwordreset');
+const messagesRoutes = require('./messages');
+const chatsRoutes = require('./chats');
+const pushyRegisterRoutes = require('./pushyregister');
+
+const middleware = require('../../middleware');
 
 router.use("/passwordreset",passwordResetRoutes)
 router.use("/register", registerRoutes);
 router.use("/signin", signInRoutes);
 router.use("/verification", verificationRoutes);
+router.use("/messages", middleware.checkToken, messagesRoutes);
+router.use("/chats", middleware.checkToken, chatsRoutes);
+router.use("/pushyregister", middleware.checkToken, pushyRegisterRoutes);
 
 module.exports = router;
 
