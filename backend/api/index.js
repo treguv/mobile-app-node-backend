f72653bd06290113f3bfbadbfc8067640411ae08
Tien Truong <thanhtien7596@gmail.com>
2021-11-21 23:11:45 -0800

    connect weather API to our API

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,6 +11,7 @@ const passwordResetRoutes = require('./passwordreset');
 const chatRoutes = require('./chats');
 const searchRoutes = require('./search');
 const contactRoutes = require('./contact');
+const weatherRoutes = require('./weather')
 
 router.use("/chat",checkToken, chatRoutes);
 router.use("/passwordreset",passwordResetRoutes);
@@ -19,6 +20,7 @@ router.use("/signin", signInRoutes);
 router.use("/verification", verificationRoutes);
 router.use("/search", searchRoutes);
 router.use("/contact", contactRoutes);
+router.use("/weather",weatherRoutes);
 
 module.exports = router;
 
---
+++ backend/api/weather.js
@@ -0,0 +1,30 @@
+const API_KEY = process.env.WEATHER_API_KEY
+const router = require("express").Router();
+const request = require('request');
+
+
+
+ router.get("/", (req, res) => {
+    let lat = 47.25
+    let lon = 122.44
+    let zip_code = 98404
+    let url = "https://api.openweathermap.org/data/2.5/weather?units=metric&zip="+ zip_code + ",us&appid=" + API_KEY;
+    request(url, function (error, response, body) {
+        if (error) {
+            res.send(error)
+        } else {
+            // pass on everything (try out each of these in Postman to see the difference)
+            // res.send(response);
+            
+            // or just pass on the body
+
+            var n = body.indexOf("{")
+            var nakidBody = body.substring(n - 1)
+
+            res.send(nakidBody)
+        }
+    })
+
+})
+
+module.exports = router
